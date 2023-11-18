435dec821c3b79841d31abd1fe42ac88d39d8d78
baltas99 <82153483+baltas99@users.noreply.github.com>
2023-11-18 18:20:01 +0200

    Update __init__.py

--- GetWeather/__init__.py
+++ GetWeather/__init__.py
@@ -1,39 +1,28 @@
 import azure.functions as func
 import requests
 import json
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # Extract the city name from the query parameters
     city = req.params.get('city')
 
     if not city:
         return func.HttpResponse(
-            "Please pass the city name in the query string",
+            "Please pass a city in the query string",
             status_code=400
         )
 
-    # Set your OpenWeatherMap API key
-    api_key = "31061e71138b8a05356fba63f13034f8"
-    # Define the base URL for the OpenWeatherMap API
+    api_key = os.environ.get('OpenWeatherMapApiKey')
     url = "https://api.openweathermap.org/data/2.5/weather"
-    # Set the parameters for the API call
     params = {"q": city, "appid": api_key, "units": "metric"}
 
-    # Make the API call to OpenWeatherMap
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
         weather_data = response.json()
-        # Create a response object with the weather data
-        weather_response = {
-            "Town": city,
-            "Temperature": f"{weather_data['main']['temp']}°C",
-            "Description": weather_data['weather'][0]['description'],
-            "Humidity": f"{weather_data['main']['humidity']}%"
-        }
-        return func.HttpResponse(json.dumps(weather_response), mimetype="application/json")
+        return func.HttpResponse(json.dumps(weather_data), mimetype="application/json")
     else:
         return func.HttpResponse(
-            f"Failed to fetch the weather data for {city}.",
+            "Failed to fetch the weather data",
             status_code=response.status_code
         )
