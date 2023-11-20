c00864bc4e5e520d7b8b13bde1908651d27f4390
baltas99 <82153483+baltas99@users.noreply.github.com>
2023-11-20 20:24:33 +0200

    Update and rename init__.py to __init__.py

--- GetWeather2/__init__.py
+++ GetWeather2/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import azure.functions as func
 import requests
+import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
